5150d044fb6858b9cd775d1d458ab9b4b1a9f033
Alex Wang <y2587wan@edu.uwaterloo.ca>
2017-11-21 22:20:58 -0500

    Maze Game with Google VR

---
+++ MazeGame/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Vector3 spawnPoint;
+    private bool isWalking = false;
+
+    private void Start()
+    {
+        spawnPoint = transform.position;
+    }
+
+    void Update () {
+        if (isWalking)
+        {
+            transform.position += Camera.main.transform.forward * 0.5f * Time.deltaTime;
+        }
+
+        if (transform.position.y < -10f)
+        {
+            transform.position = spawnPoint;
+        }
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))  
+        {
+            if (hit.collider.tag == "Ground")
+            {
+                isWalking = false;
+            }
+            else
+            {
+                isWalking = true;
+            }
+        }
+	}
+}
