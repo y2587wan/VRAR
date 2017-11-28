958718e969efe81c5d5e2ea8267a2e50bbcd67ee
Alex Wang <y2587wan@edu.uwaterloo.ca>
2017-11-27 22:04:33 -0500

    Move Camera by pressing button in VR

---
+++ VRtesting/Assets/Script/CameraMotion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour {
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.position += Camera.main.transform.forward * 1.5f * Time.deltaTime;
+        }
+    }
+}
